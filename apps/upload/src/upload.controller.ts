b8ab5586dfe69c2d86ea0805f68f2a960c0d9251
Surov Evgeniy <surovjeiya@gmail.com>
2023-02-28 15:45:40 +0300

    added upload serivce.

---
+++ apps/upload/src/upload.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+import { UploadService } from './upload.service';
+
+@Controller()
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  
+}
